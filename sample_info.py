24ce606e296756aac6b3a21eebb3aff61d540ca2
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-05-31 12:42:53 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'LuciferMoringstar_Robot'
 USER_SESSION = 'User_Bot'
-API_ID = 4546803
-API_HASH = 08ad181fba3b05e1141db96175cab60e
-BOT_TOKEN = 123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
 
 # Bot settings
